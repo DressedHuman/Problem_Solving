2bdc351aec6e65192ec52ef9ad034724bda62fc2
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-07-24 16:57:57 +0600

    removed some unused/commented lines of code from BigSumMod

--- Lessons/BigSumMod.java
+++ Lessons/BigSumMod.java
@@ -11,20 +11,11 @@ public class BigSumMod {
             return (1+a)%M;
         }
         
-        int x = big_sum_mod_recursive((a*a)%M, b/2, M); // calculating the of the part with half the power, b
+        int x = big_sum_mod_recursive((a*a)%M, b/2, M); // calculating the value of the part with half the power of b
         x = (x*(1+a))%M; // calculating the full expression with even power (even b)
         if(b%2==1){ // checking if the power, b is odd
             x = (1+(a*x))%M; // calculating the full expression with power, b being odd
         }
-        /* if(b%2==1){
-            x = big_sum_mod_recursive(a, b-1, M);
-            x = (1+(a*x))%M;
-        }
-        else{
-            x = big_sum_mod_recursive((a*a)%M, b/2, M);
-            x = x*(1+a)%M;
-        } */
-
         return x;
     }
     
